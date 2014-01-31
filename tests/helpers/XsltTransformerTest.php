b024682f62eba4e64a6eef36821fadefa26098b4
Daniel Silva <daniel.silva@flipside.org>
2014-01-31 11:22:43 +0000

    Fix #10. Adds tests to the xslt transformer. Removes old xslt transformer helper. Adds resources.

--- tests/helpers/XsltTransformerTest.php
+++ tests/helpers/XsltTransformerTest.php
@@ -1,6 +1,6 @@
 <?php
 
-class Xslt_transformer_helper_test extends PHPUnit_Framework_TestCase {
+class XsltTransformerHelperTest extends PHPUnit_Framework_TestCase {
 	  
 	private static $CI;
     
@@ -19,6 +19,37 @@ class Xslt_transformer_helper_test extends PHPUnit_Framework_TestCase {
     $xslt_transformer = new Xslt_transformer('foo.bar');
   }
   
+  /**
+   * Trying to get transformed sxe without setting a path for
+   * the enketo xslt library.
+   */
+  public function test_enketo_xslt_lib_path_not_set() {
+    $xslt_transformer = new Xslt_transformer('tests/test_resources/surveys/valid_survey.xml');
+    $result = $xslt_transformer->get_transform_result_sxe();
+    
+    $this->assertFalse($result);
+    
+    $errors = $xslt_transformer->get_errors();    
+    $this->assertCount(1, $errors['xsl_form_errors']);
+    $this->assertCount(1, $errors['xsl_data_errors']);
+  }
+  
+  /**
+   * Converting a valid file.
+   */
+  public function test_xslt_transformer_valid() {
+    $xslt_transformer = new Xslt_transformer('tests/test_resources/surveys/super_simple_survey.xml');
+    $xslt_transformer->set_enketo_xslt_lib_location('./application/third_party/enketo-xslt/');
+    $result = $xslt_transformer->get_transform_result_sxe();
+    
+    $errors = $xslt_transformer->get_errors();    
+    // Every error group should be empty.
+    foreach ($errors as $error_group) {
+      $this->assertEmpty($error_group);
+    }
+    
+    $this->assertInstanceOf('SimpleXMLElement', $result);
+  }
 }
 
 ?>
