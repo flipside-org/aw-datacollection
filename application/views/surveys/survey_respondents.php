5c4fd862a66379da3a192c6de1b136e0ba5da89f
Nuno Veloso <nunoveloso18@gmail.com>
2014-03-06 17:11:58 +0100

    added view placeholder for respondents list

---
+++ application/views/surveys/survey_respondents.php
@@ -0,0 +1,23 @@
+<div class="row">
+
+  <?= $messages ?>
+
+  <span class="label"><strong>Status:</strong> <?= $survey->status ?></span>
+  <h1><?= $survey->title ?></h1>
+  <h2>Respondents list</h2>
+
+  <div>
+    <?= nl2br_except_pre($survey->introduction) ?>
+  </div>
+
+  <?php if (has_permission('download survey files')) : ?>
+    <?php if ($survey->files['xls'] !== NULL) : ?>
+      <div>xls: <a href="<?= base_url(sprintf('survey/%d/files/xls', $survey->sid)); ?>">xls</a></div>
+    <?php endif; ?>
+
+    <?php if ($survey->files['xml'] !== NULL) : ?>
+      <div>xml: <a href="<?= base_url(sprintf('survey/%d/files/xml', $survey->sid)); ?>">xml</a></div>
+    <?php endif; ?>
+  <?php endif; ?>
+
+</div>
