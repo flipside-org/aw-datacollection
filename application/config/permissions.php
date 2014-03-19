6485cb9e1ff9d923464e1eb817e0fcf6fc220de9
Daniel Silva <daniel.silva@flipside.org>
2014-03-19 18:26:58 +0000

    Add moderator role

--- application/config/permissions.php
+++ application/config/permissions.php
@@ -13,7 +13,7 @@
 |
 | - cc_agent
 | - administrator
-|
+| - moderator
 |
 |
 |
@@ -28,30 +28,38 @@ define('ROLE_REGISTERED', 'authenticated');
 
 // Some constants to define actual roles.
 define('ROLE_ADMINISTRATOR', 'administrator');
+define('ROLE_MODERATOR', 'moderator');
 define('ROLE_CC_AGENT', 'cc_agent');
 
 $config['roles'] = array(
   ROLE_ADMINISTRATOR => 'Administrator',
+  ROLE_MODERATOR => 'Moderator',
   ROLE_CC_AGENT => 'CC Agent'
 );
 
 $config['permissions'] = array(
   // Users.
-  'edit own account' => array(ROLE_REGISTERED),
-  'edit any account' => array(ROLE_ADMINISTRATOR),
-  'create account' => array(ROLE_ADMINISTRATOR),
-  'view user list' => array(ROLE_ADMINISTRATOR),
+  'edit own account'  => array(ROLE_REGISTERED),
+  'edit any account'  => array(ROLE_ADMINISTRATOR),
+  'create account'    => array(ROLE_ADMINISTRATOR),
+  'view user list'    => array(ROLE_ADMINISTRATOR),
   
   // Surveys.
-  'view survey list' => array(ROLE_REGISTERED),
-  'view survey page' => array(ROLE_REGISTERED),
-  'edit any survey' => array(ROLE_ADMINISTRATOR),
-  'create survey' => array(ROLE_ADMINISTRATOR),
-  'delete any survey' => array(ROLE_ADMINISTRATOR),
-  'download survey files' => array(ROLE_REGISTERED),
-  'assign agents' => array(ROLE_ADMINISTRATOR),
+  'view survey list'          => array(ROLE_ADMINISTRATOR, ROLE_MODERATOR),
+  'view any survey page'      => array(ROLE_ADMINISTRATOR, ROLE_MODERATOR),
+  'view assigned survey page' => array(ROLE_CC_AGENT),
+  'edit any survey'           => array(ROLE_ADMINISTRATOR, ROLE_MODERATOR),
+  'create survey'             => array(ROLE_ADMINISTRATOR, ROLE_MODERATOR),
+  'delete any survey'         => array(ROLE_ADMINISTRATOR, ROLE_MODERATOR),
+  'download survey files'     => array(ROLE_ADMINISTRATOR, ROLE_MODERATOR, ROLE_CC_AGENT),
+  'assign agents'             => array(ROLE_ADMINISTRATOR, ROLE_MODERATOR),
   
-  'collect data with enketo' => array(ROLE_REGISTERED),
+  // Although this permission exists should no be used.
+  // It allows non assigned users to collect data which should not happen.
+  'enketo collect data any'       => array(),
+  'enketo collect data assigned'  => array(ROLE_ADMINISTRATOR, ROLE_MODERATOR, ROLE_CC_AGENT),
+  'enketo testrun any'            => array(ROLE_ADMINISTRATOR, ROLE_MODERATOR),
+  'enketo testrun assigned'       => array(ROLE_CC_AGENT),
   
-  'api request csrf token' => array(ROLE_REGISTERED),
+  'api request csrf token'    => array(ROLE_ADMINISTRATOR, ROLE_MODERATOR, ROLE_CC_AGENT),
 );
