e64df9dbf2c383a1cd4df6b1f994a9ae0a453b5e
Daniel Silva <daniel.silva@flipside.org>
2014-03-26 18:16:13 +0000

    Style buttons and dropdown. Add temporary font.

--- application/views/frontend.php
+++ application/views/frontend.php
@@ -22,9 +22,9 @@
           </ul>
         </li>
         <li>
-          <a href="#" class="account"><span class="visually-hidden">Account</span></a>
-          <ul class="action-dropdown">
-            <li>Signed in as <span>Bruce Wayne</span></li>
+          <a href="#" class="account" data-dropdown="action-primary"><span class="visually-hidden">Account</span></a>
+          <ul class="action-dropdown-primary" style="display: none">
+            <li class="logged-user">Signed in as <strong>Bruce Wayne</strong></li>
             <li><a href="#">Edit profile</a></li>
             <li><a href="#">Sign out</a></li>
           </ul>
@@ -45,7 +45,28 @@
     			<h1>Client Satisfaction Survey</h1>
     		</div>
     		<nav id="secondary" class="actions" role="navigation">
-    		  page navigation
+    		  
+    		  <ul class="links">
+    		  	<li>
+    		  	  <a href="" class="bttn bttn-danger bttn-medium has-dropdown" data-dropdown="action-bttn">Destroy the world</a>
+    		  	  <ul class="action-dropdown" style="display: none">
+                <li><a href="#">Bomb</a></li>
+                <li><a href="#">Black Hole</a></li>
+                <li><a href="#">Banana</a></li>
+              </ul>
+    		  	</li>
+    		  	<li>
+    		  	  <a href="" class="bttn bttn-success bttn-medium">Button success</a>
+    		  	</li>
+    		  	<li>
+    		  	  <a href="" class="bttn bttn-primary bttn-medium">Button primary</a>
+    		  	</li>
+    		  	<li>
+    		  	  <a href="" class="bttn bttn-primary bttn-small">Button small</a>
+    		  	</li>
+    		  </ul>
+    		  
+    		  
     		</nav>
     	</div>
     </header>
