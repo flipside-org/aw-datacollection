d94acf598ef4f4e5688ab90008af42ced4099b72
Daniel Silva <daniel.silva@flipside.org>
2014-06-12 12:42:16 +0100

    Fix #129. Wrong labels when exporting. Missing machine_label key from select questions

--- application/controllers/survey.php
+++ application/controllers/survey.php
@@ -1251,10 +1251,14 @@ class Survey extends CI_Controller {
     $flat = $flattener->get_flatten();
     $header = array();
     foreach ($flat as $key => $value) {
-      if ($flattener->is_translated()) {
-        $header[] = $value[$label_key][$flattener->get_preferred_language()];
+      // The language only matters when we're exporting a non system
+      // question in a normalised format.
+      if ($type == 'csv_human' && !isset($value['system']) && $flattener->is_translated()) {
+        $header[] = $value['label'][$flattener->get_preferred_language()];
       }
       else {
+        // Machine labels are never translated and human label behave the
+        // same way if there's no translation.
         $header[] = $value[$label_key];
       }
     }
@@ -1267,7 +1271,7 @@ class Survey extends CI_Controller {
     header("Content-Disposition: attachment; filename=" . $filename);
     header("Content-Type: application/octet-stream; "); 
     header("Content-Transfer-Encoding: binary");
-    
+
     // Open stream.
     $output = fopen('php://output', 'w');
     
