aad9244e6806d9c1bd6d434aa02c4419c0233414
Daniel Silva <daniel.silva@flipside.org>
2014-06-17 16:42:40 +0100

    Add Github ribbon

--- application/views/base/html_end.php
+++ application/views/base/html_end.php
@@ -1,4 +1,9 @@
     </div>
+  <?php if (ENVIRONMENT == 'demo') : ?>
+  <p class="ribbon">
+    <a title="The Organization Finder on Github" href="https://github.com/flipside-org/">View on GitHub</a>
+  </p>
+  <?php endif; ?>
   <?php $this->load->view('components/confirm_box') ?>
   <?php $this->load->view('base/footer_scripts') ?>
   </body>
